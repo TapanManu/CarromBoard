9e99c15e2f82a9c687780b8f67f1a1d7b93a76c5
Tapan <tapanmanu2000@gmail.com>
2020-09-23 20:32:44 +0530

    mouse pointer fixed

--- js/Coin.js
+++ js/Coin.js
@@ -1,20 +1,32 @@
-class Coin{
-	constructor(canvas,rad,color,xpos,ypos){
-		this.x=xpos;
-		this.y=ypos;
-		this.rad=rad;
-		this.color=color;
-		this.pts;			//points associated when it falls into hole
-		this.state = false;			//rest state
-		this.view();
-	}
-	view(){
-		let c = document.querySelector("canvas");
-		let ctx = c.getContext("2d");
-		ctx.beginPath();
-		ctx.fillStyle=this.color;
-		ctx.arc(this.x,this.y,this.rad,0,2*Math.PI);
-		ctx.fill();
-	}
-	
-}
+let j=0,k=0;
+let svg = document.getElementById('svg8');
+let striker = svg.getElementById('striker');
+let clicked=false;
+window.addEventListener("load", function(event) {
+      let elip = svg.getElementById('redPuc');
+      striker.onmousedown = function(event){
+      let shiftX = event.clientX - striker.getBoundingClientRect().left;
+      let shiftY = event.clientY - striker.getBoundingClientRect().top;
+      let l=0,t=0;
+      clicked=!clicked;
+      striker.style.position = 'relative';
+      function onMouseMove(event) {        
+        console.log(event);
+        var CTM = svg.getScreenCTM();
+        if(clicked)
+      		striker.setAttribute('cx',(event.clientX-CTM.e)/CTM.a);      	
+  
+      }
+      svg.addEventListener('mousemove', onMouseMove);
+      svg.appendChild(striker);
+
+      striker.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        striker.onmouseup = null;
+      };
+      
+      }
+
+      
+    
+  });
--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,1 @@
-let board = new Board(500,500);
-board.view();
 
