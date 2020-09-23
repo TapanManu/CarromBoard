69c2ff8136b108272301a66ac301733a283b5a31
Tapan <tapanmanu2000@gmail.com>
2020-09-23 21:39:45 +0530

    init movement

--- js/Coin.js
+++ js/Coin.js
@@ -1,20 +1,28 @@
-let j=0,k=0;
+let j=0;
 let svg = document.getElementById('svg8');
 let striker = svg.getElementById('striker');
 let clicked=false;
 window.addEventListener("load", function(event) {
       let elip = svg.getElementById('redPuc');
       striker.onmousedown = function(event){
-      let shiftX = event.clientX - striker.getBoundingClientRect().left;
-      let shiftY = event.clientY - striker.getBoundingClientRect().top;
-      let l=0,t=0;
       clicked=!clicked;
       striker.style.position = 'relative';
+      striker.setAttribute('cx',250);
+      striker.setAttribute('cy',310);
+      j++;
       function onMouseMove(event) {        
-        console.log(event);
+        //console.log(event);
         var CTM = svg.getScreenCTM();
-        if(clicked)
-      		striker.setAttribute('cx',(event.clientX-CTM.e)/CTM.a);      	
+        let x = (event.clientX-CTM.e)/CTM.a;
+        let y = (event.clientY-CTM.f)/CTM.d;
+        if(clicked){    	
+        	if(x>=110 && x<=320)
+      			striker.setAttribute('cx',x);      	//mouse pointer move along striker
+        }
+        else if(!clicked && j>=2){
+        		striker.setAttribute('cx',100);
+        		striker.setAttribute('cy',200);
+        }
   
       }
       svg.addEventListener('mousemove', onMouseMove);
@@ -25,8 +33,5 @@ window.addEventListener("load", function(event) {
         striker.onmouseup = null;
       };
       
-      }
-
-      
-    
+      }   
   });
