ac5ddcddcfd8636ee5333acc403e8d642abb8dbf
Tapan <tapanmanu2000@gmail.com>
2020-09-25 21:33:59 +0530

    Revert "rebound from walls"
    This reverts commit 904bd540bb8f0a3979e2a8990d66e1e77fd2d41a.

--- js/Coin.js
+++ js/Coin.js
@@ -7,10 +7,8 @@ let cx_right_wall = 405;
 let top_wall = 25;
 let p1 = new Point(250,310);
 let p2 = new Point(cx_left_wall,190);
-let p3 = new Point(cx_right_wall,188);
-let p4 = new Point(230,top_wall);
 
-
+drawLine(p1,p2);
 //drawLine(p2,p3);
 window.addEventListener("load", function(event) {
       let elip = svg.getElementById('redPuc');
@@ -24,14 +22,14 @@ window.addEventListener("load", function(event) {
       function onMouseMove(event) {        
         //console.log(event);
         var CTM = svg.getScreenCTM();
-        let x = (event.clientX-CTM.e)/CTM.a;		//mouse drag issue fixed
+        let x = (event.clientX-CTM.e)/CTM.a;
         let y = (event.clientY-CTM.f)/CTM.d;
         if(clicked){    	
         	if(x>=110 && x<=320)
       			striker.setAttribute('cx',x);      	//mouse pointer move along striker
         }
         else if(!clicked && j>=2){
-        		rebound_from_wall(p4);
+        		rebound_from_wall(p2);
         }
   		
       }
--- js/Force.js
+++ js/Force.js
@@ -14,23 +14,16 @@ function dist(p1,p2){
 }
 
 function rebound_from_wall(p){
-	drawLine(p1,p);
 	striker.setAttribute('cx',p.x);
 	striker.setAttribute('cy',p.y);
-	let x1,y1;
-	if(p.x==22 || p.x==405){
-		length=dist(new Point(p1.x,p.y),p1);
-		console.log(length);
-		x1 = p1.x;
-		y1 = p1.y - 2*length;
-	}
-	else if(p.y==25){
-		length = dist(new Point(p.x,p1.y),p1);
-		console.log(length);
-		x1 = p1.x - 2*length;
-		y1 = p1.y;
-	}
-	drawLine(p,new Point(x1,y1));
+
+	length=dist(new Point(p1.x,p2.y),p1);
+	console.log(length);
+	let x1 = p1.x;
+	let y1 = p1.y - 2*length;
+	//console.log(x1);
+	//console.log(y1);
+	drawLine(p2,new Point(x1,y1));
 	striker.setAttribute('cx',x1);
 	striker.setAttribute('cy',y1);
 	return;
